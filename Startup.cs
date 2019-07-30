54f44b64f368ca2308ff2bf79fe41dd0772a6ede
SankarKM <53461761+SankarKM@users.noreply.github.com>
2019-07-30 12:09:32 +0530

    Startup and Appsettings
    Controller  Startup and Appsettings

---
+++ Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IHMS.Data;
+using IHMS.Data.Model;
+using IHMS.Data.Repository;
+using IHMS.Data.Repository.Implementation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace WebApiCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var conString = Configuration.GetConnectionString("IHMSContext");
+            var emrConString = Configuration.GetConnectionString("EMRContext");
+
+
+
+            services.AddCors(x => { x.AddPolicy("AllowAll", y => y.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials().Build()); });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+            services.AddDbContext<IHMSContext>(Options => Options.UseSqlServer(conString));
+            services.AddDbContext<EMRContext>(Options => Options.UseSqlServer(emrConString));
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            services.AddMvc().AddJsonOptions(x =>
+            {
+                x.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                x.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowAll");
+            app.UseAuthentication();
+
+
+            app.UseMvc();
+        }
+    }
+}
