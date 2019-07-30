acfc1c3d8453c2ee14fbe2b8f40c0ba1da6786dd
SankarKM <53461761+SankarKM@users.noreply.github.com>
2019-07-30 12:04:35 +0530

    Common Dropdown
    Common Dropdown

---
+++ Dropdown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IHMS.Data.Common
+{
+    public class Dropdown
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+}
